c712afbf2493967c39b6f2b34860545842d726a7
Bumba-jr <iliyashatami20@gmail.com>
2025-01-16 12:07:59 +0100

    google_Font

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 
-const Home = () => {
-  return (
-    <h1>Hello World </h1>
-  );
-}
-
+const Home = () => (
+  <>
+    <h1 className="text-3xl text-slate-500 font-black">
+      welcome to the world of NExt.js Hello World{" "}
+    </h1>
+    <h1 className="text-3xl text-slate-500 font-black font-space-grotesk">
+      welcome to the world of NExt.js Hello World{" "}
+    </h1>
+  </>
+);
 export default Home;
